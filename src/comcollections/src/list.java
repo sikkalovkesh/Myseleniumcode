3457029cb9df433dbe40d7b27079bdf5f67ca7a2
lovkesh <sikkalovkesh@gmail.com>
2021-05-30 22:48:13 +0530

    my code is going to update the git

--- src/comcollections/src/list.java
+++ src/comcollections/src/list.java
@@ -7,24 +7,33 @@ public class list {
 	
 	public static void main(String[] args) {
 		//simple code for list
-//		List<String> li=new ArrayList<String>();
-//		li.add("apple");
-//		li.add("orange");
-//		li.add("pineapple");
-//		
-//		for(String fruits:li) {
-//			System.out.println(fruits);
-//		}
-//		
-		String arr[]= {"lovkesh","sikka","software","testengineer"};
-		System.out.println("print array:-"+Arrays.toString(arr));
-		
 		List<String> li=new ArrayList<String>();
-		for(String info: arr) {
-			li.add(info);
-		}
-		System.out.println("print array to list:-"+li);
+		li.add("apple");
+		li.add("orange");
+		li.add("pineapple");
 		
+		for(String fruits:li) {
+			System.out.println(fruits);
+		}
+		
+		System.out.println("Return the data:-"+li.get(1));
+		
+		li.set(1, "watermelon");
+		System.out.println("print the update data:-"+li);
+		
+		Collections.sort(li);
+		System.out.println(li);
+		
+//		//array to list
+//		String arr[]= {"lovkesh","sikka","software","testengineer"};
+//		System.out.println("print array:-"+Arrays.toString(arr));
+//		
+//		List<String> li=new ArrayList<String>();
+//		for(String info: arr) {
+//			li.add(info);
+//		}
+//		System.out.println("print array to list:-"+li);
+//		
 		
 		
 	}
