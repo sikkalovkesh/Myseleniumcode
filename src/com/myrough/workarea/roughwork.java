7297ddce66edc45925b3794dea4d4b68f3b45382
lovkesh <sikkalovkesh@gmail.com>
2021-06-01 22:14:30 +0530

    my updated code

--- src/com/myrough/workarea/roughwork.java
+++ src/com/myrough/workarea/roughwork.java
@@ -7,12 +7,7 @@ public class roughwork {
 	
 	
 	public static void main(String[] args) {
-		
-		HashMap<Integer, String> hm=new HashMap<Integer, String>();
-		hm.put(1, "Lovkesh");
-		hm.put(2, "sikka");
-		
-		System.out.println(hm);
+
 		
 		
 		
